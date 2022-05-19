d4c4ab903a9978f096364d4f8581e88598ade4f7
Daoud Merchant <merchantdaoud@hotmail.com>
2022-05-19 21:44:28 +0200

    Add logic, transitions, basic styling

--- static/index.js
+++ static/index.js
@@ -1,108 +1,146 @@
 // PRIME FACTORS
 
-const errors = [{
-    check: num => typeof num !== "number",
-    msg: "Must be a number"
-},
-{
-    check: num => num === 0,
-    msg: "Number must not be 0"
-},
-{
-    check: num => num < 1,
-    msg: "Number cannot be negative"
-}]
+const errors = [
+  {
+    check: (num) => typeof num !== "number",
+    msg: "Must be a number",
+  },
+  {
+    check: (num) => num === 0,
+    msg: "Number must not be 0",
+  },
+  {
+    check: (num) => num < 1,
+    msg: "Number cannot be negative",
+  },
+];
 
 const primeFactor = (num) => {
-    for (let {check, msg} of errors) {
-        if (check(num)) return new Error(msg)
-    }
-    if (num === 1) return [];
+  for (let { check, msg } of errors) {
+    if (check(num)) return new Error(msg);
+  }
+  if (num === 1) return [];
 
-    let factors = [];
-    let divisor = 2;
+  let factors = [];
+  let divisor = 2;
 
-    while (num > 1) {
-        if (num % divisor === 0) {
-            factors.push(divisor);
-            num /= divisor
-        } else {
-            divisor++
-        }
+  while (num > 1) {
+    if (num % divisor === 0) {
+      factors.push(divisor);
+      num /= divisor;
+    } else {
+      divisor++;
     }
-    return factors;
-}
+  }
+  return factors;
+};
 
 // FIZZBUZZ
 
-const evaluate = value => {
-    if (typeof value !== 'number') {
-      return 'Please provide a number. Try again';
-    }
-    if (value < 1) {
-      return 'Please provide a positive number. Try again';
-    }
-    if (value > 100) {
-      return 'Please provide a number below 100';
-    }
-    let text = '';
-    if (value % 3 === 0) {
-      text += 'Fizz';
-    }
-    if (value % 5 === 0) {
-      text += 'Buzz';
-    }
-    return text || value;
-  };
-  
-  const fizzBuzz = array => {
-    if (!Array.isArray(array)) {
-      return new Error("Must be an array!");
-    }
-    return array.map(evaluate);
-  };
+const evaluate = (value) => {
+  if (typeof value !== "number") {
+    return "Please provide a number. Try again";
+  }
+  if (value < 1) {
+    return "Please provide a positive number. Try again";
+  }
+  if (value > 100) {
+    return "Please provide a number below 100";
+  }
+  let text = "";
+  if (value % 3 === 0) {
+    text += "Fizz";
+  }
+  if (value % 5 === 0) {
+    text += "Buzz";
+  }
+  return text || value;
+};
+
+const fizzBuzz = (array) => {
+  if (!Array.isArray(array)) {
+    return new Error("Must be an array!");
+  }
+  return array.map(evaluate);
+};
 
 // DOM queries
 
-const getDomElements = id => ({
-    button: document.querySelector(`button.${id}`),
-    section: document.querySelector(`#${id}`),
-    form: document.querySelector(`#${id} form`),
-    input: document.querySelector(`#${id} input`),
-    error: document.querySelector(`#${id} .error`),
-    output: document.querySelector(`#${id} .output`)
-})
+const getDomElements = (id) => ({
+  button: document.querySelector(`button.${id}`),
+  section: document.querySelector(`#${id}`),
+  form: document.querySelector(`#${id} form`),
+  input: document.querySelector(`#${id} input`),
+  error: document.querySelector(`#${id} .error`),
+  output: document.querySelector(`#${id} .output`),
+});
 
 const fizz = getDomElements("fizzbuzz");
-const prime = getDomElements("primefactors")
+const prime = getDomElements("primefactors");
 
-fizz.button.addEventListener("click", () => {
-    prime.section.classList.add("hidden");
-    fizz.section.classList.remove("hidden");
-})
+const addViewToggle = (proj1, proj2) => {
+  const addListeners = (a, b) => {
+    a.button.addEventListener("click", () => {
+      b.section.classList.add("fadeout");
+      setTimeout(() => {
+        b.section.classList.add("hidden");
+        a.section.classList.remove("hidden");
+      }, 300);
+      setTimeout(() => {
+        a.section.classList.remove("fadeout");
+      }, 400);
+    });
+  };
+  addListeners(proj1, proj2);
+  addListeners(proj2, proj1);
+};
 
-prime.button.addEventListener("click", () => {
-    fizz.section.classList.add("hidden");
-    prime.section.classList.remove("hidden");
-})
+addViewToggle(fizz, prime);
 
-const phraseFactors = arr => {
-    return arr.join(", ").replace(/, (?!.*, )/, " and ")
-}
-
-prime.form.addEventListener("submit", e => {
+const addFormFunctionality = (proj, callFunc, getText) => {
+  proj.form.addEventListener("submit", (e) => {
     e.preventDefault();
-    prime.error.innerHTML = ""
-    const numberInput = +prime.input.value;
-    const param = Number.isFinite(numberInput) ? numberInput : prime.input.value;
-    const factors = primeFactor(param)
-    if (factors instanceof Error) {
-        prime.error.innerHTML = `<p>${factors.toString()}</p>`;
-        prime.output.innerHTML = "";
-        return;
+    proj.error.innerHTML = "";
+    const input = +proj.input.value;
+    const value = Number.isFinite(input) ? input : proj.input.value;
+    const result = callFunc(value);
+    if (result instanceof Error) {
+      proj.output.innerHTML = "";
+      proj.error.innerHTML = `<p>${result.toString()}</p>`;
+      return;
     }
-    const sentence = `The prime factors are ${phraseFactors(factors)}`;
-    prime.output.innerHTML = `<p>${sentence}</p>`
-    prime.input.value = "";
-    prime.error.innerHTML = "";
-})
+    const text = getText(result);
+    proj.output.innerHTML = text;
+  });
+};
+
+const formatPrimeFactors = (factorArr) => {
+  if (!factorArr.length) return "<p>There are no prime factors!</p>";
+  return `The prime factors are ${factorArr
+    .join(", ")
+    .replace(/, (?!.*, )/, " and ")}.`;
+};
+
+addFormFunctionality(prime, primeFactor, formatPrimeFactors);
+
+const callFizzBuzzWithArray = (input) => {
+  console.log("Calling");
+  if (typeof input !== "number") return fizzBuzz(input);
+  const numArray = new Array(input).fill().map((a, i) => i + 1);
+  return fizzBuzz(numArray);
+};
+
+const formatFizzBuzz = (fizzBuzzArray) =>
+  fizzBuzzArray.map((string) => `<p>${string}</p>`).join("");
+
+addFormFunctionality(fizz, callFizzBuzzWithArray, formatFizzBuzz);
+
+// //doesnt work when element in the array is non-number (eg. "string")
+// fizz.form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const stringToArray = fizz.input.value.split(",");
+//   const newArray = stringToArray.map((item) => +item);
+//   const output = fizzBuzz(newArray);
+//   fizz.output.innerHTML = `<p>${output.join("<br>")}</p>`;
+//   fizz.input.value = "";
+// });
